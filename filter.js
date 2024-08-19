70f210cdaa34f72a2d7dfaa71f5622115cd15bf3
Sean Patton <seanpatton1@outlook.com>
2024-08-19 20:41:57 +0000

    filter method

--- filter.js
+++ filter.js
@@ -22,6 +22,14 @@ const people = [
 ];
 
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+// const paul = people.filter(person => person.name === 'Paul');
+// below gets it to print out just the object rather than returning array
+const paul = people.filter(person => person.name === 'Paul')[0];
+console.log(paul);
+
 // Complex Filtering
 const students = [
   {
@@ -55,3 +63,20 @@ const students = [
     ]
   },
 ];
+
+// const cadidates = students.filter(student =>{
+//   let strongskills = student.skills.filter(skill => skill.yrsExpirience >= 5);
+//   return strongskills.length > 0;
+// })
+// console.log(cadidates)
+
+// below is a more condensed filter
+
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+let hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const cadidates = students.filter(hasStrongSkills);
+
+const namesOfCandidates = students.filter(hasStrongSkills).map(student => student.name);
+
+console.log(namesOfCandidates);
